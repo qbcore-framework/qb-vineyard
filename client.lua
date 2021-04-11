633acfc48da3a2d5b810de91c9b2cb9a3c062b8f
GhzGarage <qbcore@programmer.net>
2021-04-10 22:49:13 -0500

    Keys Stuff

--- client.lua
+++ client.lua
@@ -1,17 +1,5 @@
 QBCore = nil
 
-local Keys = {
-	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}
-
 Citizen.CreateThread(function()
 	while QBCore == nil do
 		TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
@@ -241,7 +229,7 @@ Citizen.CreateThread(function()
 							if not loadIngredients then
 								if #(pos - vector3(Config.Vineyard["wine"].coords.x, Config.Vineyard["wine"].coords.y, Config.Vineyard["wine"].coords.z)) <  )then
 									DrawText3Ds(Config.Vineyard["wine"].coords.x, Config.Vineyard["wine"].coords.y,  Config.Vineyard["wine"].coords.z + 0.2, '[E] Load Ingredients')
-									if IsControlJustPressed(0, Keys["E"]) then
+									if IsControlJustPressed(0, 38) then
 										if PlayerJob.name == "vineyard" then
 											TriggerServerEvent("qb-vineyard:server:loadIngredients")
 										else
@@ -253,7 +241,7 @@ Citizen.CreateThread(function()
 								if not finishedWine then
 									if #(pos - vector3(Config.Vineyard["wine"].coords.x, Config.Vineyard["wine"].coords.y, Config.Vineyard["wine"].coords.z)) < 1 then
 										DrawText3Ds(Config.Vineyard["wine"].coords.x, Config.Vineyard["wine"].coords.y, Config.Vineyard["wine"].coords.z + 0.2, '[E] Start WineProcess')
-										if IsControlJustPressed(0, Keys["E"]) then
+										if IsControlJustPressed(0, 38) then
 											if PlayerJob.name == "vineyard" then
 												StartWineProcess()
 											else
@@ -264,7 +252,7 @@ Citizen.CreateThread(function()
 								else
 									if #(pos - vector3(Config.Vineyard["wine"].coords.x, Config.Vineyard["wine"].coords.y, Config.Vineyard["wine"].coords.z)) <  )then
 										DrawText3Ds(Config.Vineyard["wine"].coords.x, Config.Vineyard["wine"].coords.y, Config.Vineyard["wine"].coords.z + 0.2, '[E] Get Wine')
-										if IsControlJustPressed(0, Keys["E"]) then
+										if IsControlJustPressed(0, 38) then
 											if PlayerJob.name == "vineyard" then
 												TriggerServerEvent("qb-vineyard:server:receiveWine")
 												finishedWine = false
@@ -305,7 +293,7 @@ Citizen.CreateThread(function()
 					
 					if #(pos - vector3(Config.Vineyard["grapejuice"].coords.x, Config.Vineyard["grapejuice"].coords.y, Config.Vineyard["grapejuice"].coords.z)) <  )then
 						DrawText3Ds(Config.Vineyard["grapejuice"].coords.x, Config.Vineyard["grapejuice"].coords.y,  Config.Vineyard["grapejuice"].coords.z + 0.2, '[E] Make Grape Juice')
-						if IsControlJustPressed(0, Keys["E"]) then
+						if IsControlJustPressed(0, 38) then
 							if PlayerJob.name == "vineyard" then
 								TriggerServerEvent("qb-vineyard:server:grapeJuice")
 							else
