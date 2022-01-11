04b7a8fc5a3701e0c865619eb176c77490c0ccc8
unawarepuma <97047857+unawarepuma@users.noreply.github.com>
2022-01-11 17:20:10 +0000

    It translation

---
+++ locales/it.lua
@@ -0,0 +1,21 @@
+local Translations = {
+    error = {
+        ["invalid_job"] = "Non penso di lavorare qui...",
+        ["invalid_items"] = "Non hai gli oggetti corretti!",
+        ["no_items"] = "Non hai nessun oggetto!",
+    },
+    progress = {
+        ["pick_grapes"] = "Raccogliendo l'uva ..",
+        ["process_grapes"] = "Lavorando l'uva ..",
+    },
+    task = {
+        ["start_task"] = "[E] Per Cominciare",
+        ["load_ingrediants"] = "[E] Inserisci Ingredienti",
+        ["wine_process"] = "[E] Avvia lavorazione vino",
+        ["get_wine"] = "[E] Prendi il vino",
+        ["make_grape_juice"] = "[E] Fai succo d'uva",
+        ["countdown"] = "Tempo rimasto %{time}s",
+        ['cancel_task'] = "Hai cancellato il compito"
+    }
+}
+Lang = Locale:new({phrases = Translations, warnOnMissing = true})
