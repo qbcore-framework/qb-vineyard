b0af1fff0493d057e83601c3e921521fd2755157
Marcell <chillcode.codersparadise@gmail.com>
2024-09-12 18:07:14 +0200

    added missing variables and fixed spelling errors

--- locales/de.lua
+++ locales/de.lua
@@ -1,21 +1,29 @@
 local Translations = {
     error = {
-        ["invalid_job"] = "Ich denke ich Arbeite hier nicht...",
-        ["invalid_items"] = "Du hast nicht die korrekten Gegenstände!",
+        ["invalid_job"] = "Ich glaube nicht, dass ich hier arbeite...",
+        ["invalid_items"] = "Du hast nicht die richtigen Gegenstände!",
         ["no_items"] = "Du hast keine Gegenstände!",
     },
     progress = {
-        ["pick_grapes"] = "Sammle Weintrauben ..",
-        ["process_grapes"] = "Verarbeite Weintrauben ..",
+        ["pick_grapes"] = "Trauben pflücken...",
+        ["process_grapes"] = "Trauben verarbeiten...",
     },
     task = {
-        ["start_task"] = "[E] zum Starten",
+        ["start_task"] = "[E] Um zu starten",
         ["load_ingrediants"] = "[E] Zutaten laden",
-        ["wine_process"] = "[E] Starte den Prozess",
+        ["wine_process"] = "[E] Weinherstellung starten",
         ["get_wine"] = "[E] Wein nehmen",
-        ["make_grape_juice"] = "[E] Mache Wein",
-        ["countdown"] = "Zeit übrig %{time}",
-        ['cancel_task'] = "Du hast die Aufgabe abgebrochen"
+        ["make_grape_juice"] = "[E] Wein herstellen",
+        ["countdown"] = "Verbleibende Zeit %{time}s",
+        ['cancel_task'] = "Du hast die Aufgabe abgebrochen",
+    },
+    text = {
+        ["start_shift"] = "Du hast deine Schicht im Weingut begonnen!",
+        ["end_shift"] = "Deine Schicht im Weingut ist beendet!",
+        ["valid_zone"] = "Gültige Zone!",
+        ["invalid_zone"] = "Ungültige Zone!",
+        ["zone_entered"] = "%{zone}-Zone betreten",
+        ["zone_exited"] = "%{zone}-Zone verlassen",
     }
 }
 
